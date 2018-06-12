fb6cef5a16200d3c7c44435e4a21af017184f7bb
letoan96 <letranthetoan@gmail.com>
2018-06-12 17:45:14 +0700

    make it better 2

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'password_resets/new'
 
   get 'password_resets/edit'
-  get '/freechat', to: 'rooms#show'
+  root 'rooms#show'
   get  '/signup',  to: 'users#new'
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
